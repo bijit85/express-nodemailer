7ebd5893b5f7a80cc6c265d1fbff4c4f0e2896bf
Bijit Kundu <bijit.kundu@yahoo.com>
2021-12-23 14:43:30 +0800

    first commit

---
+++ routes/contact.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var nodemailer = require('nodemailer');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('contact', { title: 'Contact' });
+});
+
+router.post('/send', function (req,res, next) {
+    var trasnporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'thisisme1985@gmail.com',
+            pass: 'irjfdvsicuodznmt'
+        }
+    });
+    var mailOptions = {
+        from: 'Express Test<thisisme1985@gmail.com>',
+        to: 'thisisme1985@gmail.com',
+        subject: 'Website submission',
+        text: 'You have a new submission with the following details...Name: '+req.body.name+' Email:'+req.body.email+' Message:'+req.body.message,
+        html: '<p>You got a new submission with the following details..</p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message '+req.body.message+'</li></ul>'
+    };
+    trasnporter.sendMail(mailOptions, function (error, info) {
+        if(error) {
+            console.log(error);
+            res.redirect('/');
+        } else {
+            console.log('Message sent: '+info.response);
+            res.redirect('/');
+        }
+    })
+});
+
+module.exports = router;
